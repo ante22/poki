3084399265f8161d7f91436b8b2e22d480b76d60
ante22 <antezecevic5@gmail.com>
2024-12-14 18:54:38 +0100

    dobivamo 20 pokemona

--- poki.js
+++ poki.js
@@ -1,2 +1,12 @@
 var url = "https://pokeapi.co/api/v2/pokemon-color/yellow";
-fetch(url).then((color) => color.json);
+fetch(url)
+  .then((color) => color.json())
+  .then((yellow) => {
+    const yellowPokemon = yellow.pokemon_species.slice(0, 20);
+    yellowPokemon.forEach((pokemon) => {
+      console.log(pokemon.name);
+    });
+  })
+  .catch((error) => {
+    console.error("Problem with your fetch operation:", error);
+  });
